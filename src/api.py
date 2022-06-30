3d3db35aeca7b1a8afe68761be62252da81dbb9d
Dylan Green <cobaltgoldcsprinks@gmail.com>
2022-06-29 23:05:51 -0600

    Add docstrings and slightly tidy up codebase

--- src/api.py
+++ src/api.py
@@ -9,11 +9,27 @@ import requests
 import structures
 
 
+
 def requestJSON(token: str, url) -> Optional[str]:
+    """
+    Send a request using the (access) token + url and get a json response back.
+
+    parameters:
+        token: The access token
+        url:   The api endpoint to get the json from
+
+    returns: Either the content in json format, or None if nothing was found
+    """
     if (message := requests.get(url + f"?access_token={token}")).status_code == 200:
         return message.json()
     return None
-def getOrCreateFile() -> str:
+
+def accessToken() -> str:
+    """
+    Either gets the accesstoken from 'accesstoken.txt', or asks the user for the token and creates the aforementioned file.
+
+    returns: The access token
+    """
     tokenpath = Path("accesstoken.txt")
     if not Path.exists(tokenpath):
         token = input("Please provide your access token: ")
@@ -25,6 +41,14 @@ def getOrCreateFile() -> str:
     return token
 
 def getCourseIds(token: str) -> list[int]:
+    """
+    Gets a list of current course ids for the current user.
+
+    parameters:
+        token: The access token
+
+    returns: A list of current course ids
+    """
     coursesurl = f"https://canvas.instructure.com/api/v1/courses"
     jsoncourses = requestJSON(token, coursesurl)
 
@@ -32,12 +56,29 @@ def getCourseIds(token: str) -> list[int]:
     return ids
 
 def getUserId(token: str) -> int:
+    """
+    Gets the current user id
+
+    parameters:
+        token: The access token
+
+    returns: The current user id
+    """
     coursesurl = f"https://canvas.instructure.com/api/v1/courses"
     jsoncourses = requestJSON(token, coursesurl)
     return jsoncourses[0]["enrollments"][0]["user_id"]
 
 
-def getUpcomingAssignmentsFromCourseId(token: str, *courseIds : int) -> list[structures.Assignment]:
+def upcomingAssignments(token: str, *courseIds : int) -> list[structures.Assignment]:
+    """
+    Gets a list of structures.Assignments based on the upcoming assignments.
+
+    parameters:
+        token: The access token
+        courseIds: One or more course ids
+
+    returns: A list of structures.Assignments based on the upcoming assignments
+    """
     assignmentList = []
     for courseId in courseIds:
 
@@ -68,11 +109,27 @@ def getUpcomingAssignmentsFromCourseId(token: str, *courseIds : int) -> list[str
     return assignmentList
 
 def todoItems(token: str):
+    """
+    Gets a list of todo items for the current user
+
+    parameters:
+        token: The access token
+
+    returns: A List of todos for the current user
+    """
     url = f"https://canvas.instructure.com/api/v1/users/self/todo"
     todoJson = requestJSON(token, url)
     print(json.dumps(todoJson, indent=4, sort_keys=True))
 
 def upcomingEvents(token: str):
+    """
+    Gets a list of upcoming events for the current user
+
+    parameters:
+        token: The access token
+
+    returns: A List of todos for the current user
+    """
     url = f"https://canvas.instructure.com/api/v1/users/self/upcoming_events"
     upcomingJson = requestJSON(token, url)
     
@@ -81,10 +138,10 @@ def upcomingEvents(token: str):
 
 
 if __name__ == "__main__":
-    token = getOrCreateFile()
+    token = accessToken()
     userid = getUserId(token)
     courseIds = getCourseIds(token)
-    print(getUpcomingAssignmentsFromCourseId(token, courseIds))
+    print(upcomingAssignments(token, courseIds))
 
 # due_at
 # allowed_attempts
